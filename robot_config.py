052b92fa9ed8cb87ace9e79fec2f47b655935fa8
Charles Yang <mryang@minelandcn.com>
2017-09-11 20:37:17 +0800

    Create robot_config.py

---
+++ robot_config.py
@@ -0,0 +1,18 @@
+version = '0.1.0'
+author = 'CharlesYang'
+qq = ''
+gtk = ''
+qztoken = ''
+url = 'https://user.qzone.qq.com/proxy/domain/taotao.qzone.qq.com/cgi-bin/emotion_cgi_publish_v6'
+
+cookie = ''
+
+debug_mode = True
+
+weather_apiurl = 'http://api.openweathermap.org/data/2.5/forecast?'
+weather_apikey = ''
+weather_apicityid = '1815286'
+
+yiju_mode_cn = True
+yiju_cn = 'http://yiju.ml/api/word.php'
+yiju_en = 'http://open.iciba.com/dsapi/'
